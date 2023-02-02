1fbdc9c280288ea2964eacc8e817b46756cc757e
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-02-02 23:18:57 +0600

    ngo page added

--- src/pages/FirstSection/FirstSection.js
+++ src/pages/FirstSection/FirstSection.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./FirstSection.css";
 import childVideo from "../../Components/assets/child help/videoChild.jpg";
 import background from "../../Components/assets/backgroundImage/background.png";
+// import childVideo from '../../assets/child help/videoChild.jpg';
+// import background from '../../assets/backgroundImage/background.png';
 const FirstSection = () => {
   const video = (
     <svg
@@ -22,7 +24,7 @@ const FirstSection = () => {
   return (
     <div className=" hidden lg:block">
       <div
-        className="grid mt-[600px] items-center gap-5 p-10 lg:grid-cols-2 md:grid-cols-2 grid-cols-1 w-[90%] bg-[#3cf09c]  mx-auto mb-[150px] custom"
+        className="grid mt-[600px] items-center gap-5 p-10 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 w-[90%] bg-[#3cf09c]  mx-auto mb-[150px] custom"
         style={{
           backgroundImage: `linear-gradient(#0b0b0b93,#0b0b0b93),url(${background})`,
           backgroundSize: "cover",
@@ -30,10 +32,10 @@ const FirstSection = () => {
         }}
       >
         <div className="indicator">
-          <span className="indicator-item cursor-pointer left-[110px] right-0 h-[30%] w-[10%] rounded-full indicator-middle badge bg-white hover:bg-[#202124] hover:text-[white] hover:border-none text-black video-icons-animations">
+          <span className="indicator-item cursor-pointer left-[90px] right-0 h-[30%] w-[8%] rounded-full indicator-middle badge bg-white hover:bg-[#202124] hover:text-[white] hover:border-none text-black video-icons-animations">
             {video}
           </span>
-          <div className="grid w-[50%]  place-items-center">
+          <div className="grid w-[40%]   place-items-center">
             <img className="readius-child" src={childVideo} alt="" />
           </div>
         </div>
@@ -41,21 +43,15 @@ const FirstSection = () => {
           <h2 className="text-white text-4xl">
             We're a worldwide nonprofit charity organization
           </h2>
-          <p className="text-white text-lg font-semibold mt-5">
-            The purpose of our organization is to help the helpless and poor
-            people. All of us workers have been working successfully. We do not
-            keep any profit for us through this organization. If you want to know
-            more about us, you can see our fund and our donations
-          </p>
         </div>
-        {/* <div className="">
+        <div className="">
           <p className="text-white text-lg font-semibold">
             The purpose of our organization is to help the helpless and poor
             people. All of us workers have been working successfully. We do not
             keep any profit for us through this organization. If you want to know
             more about us, you can see our fund and our donations
           </p>
-        </div> */}
+        </div>
       </div>
     </div>
   );
